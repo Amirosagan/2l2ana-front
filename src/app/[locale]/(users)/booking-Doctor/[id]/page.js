28ba494af9759f115078ba9901f1153fb9406878
Omar Abdelsalam <omar3328101@gmail.com>
2024-09-15 03:42:04 +0200

    user id

--- src/app/[locale]/(users)/booking-Doctor/[id]/page.js
+++ src/app/[locale]/(users)/booking-Doctor/[id]/page.js
@@ -20,56 +20,7 @@ async function getDoctorData(id) {
   }
 }
 
-export async function generateStaticParams() {
-  try {
-    const response = await axios.get('https://api.2l2ana.com/api/Doctor', {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "*/*",
-        "Cache-Control": "no-cache",
-        Pragma: "no-cache",
-        Expires: "0",
-      },
-    });
-    const doctors = response.data;
 
-    const paths = doctors.map((doctor) => ({
-      id: doctor.id.toString(),
-    }));
-
-    return paths;
-  } catch (error) {
-    console.error("Failed to generate static params:", error);
-    return [];
-  }
-}
-
-export async function generateMetadata({ params }) {
-  const { id } = params;
-  const doctor = await getDoctorData(id);
-
-  return {
-    metadataBase: new URL('https://2l2ana.com'),
-    title: `قلقانة | ${doctor?.name || 'Doctor Details'}`,
-    description: doctor?.description || "احصل على استشارة طبية من أفضل الأطباء المتخصصين.",
-    keywords: "تفاصيل الطبيب, استشارة طبية, أطباء متخصصون, حجز استشارة, معلومات الطبيب",
-    author: "قلقانة",
-    openGraph: {
-      title: `تفاصيل الطبيب | ${doctor?.name || 'Doctor Details'}`,
-      description: doctor?.description || "احصل على استشارة طبية من أفضل الأطباء المتخصصين.",
-      type: 'profile',
-      url: `https://2l2ana.com/doctor/${id}`,
-      images: [
-        {
-          url: doctor?.imageUrl || 'default-image-url.jpg',
-          width: 800,
-          height: 600,
-          alt: doctor?.name || 'Doctor',
-        },
-      ],
-    },
-  };
-}
 
 const DoctorDetailsPage = async ({ params }) => {
   const { id } = params;
@@ -82,14 +33,14 @@ const DoctorDetailsPage = async ({ params }) => {
   return (
     <div dir="rtl" className="p-5 -mt-5 md:mt-0 md:px-32">
       <h2 className="tajawal-bold text-[22px] hidden md:block text-primary">تفاصيل</h2>
-      <div className="grid grid-cols-1 lg:grid-cols-9 gap-5">
+      {/* <div className="grid grid-cols-1 lg:grid-cols-9 gap-5">
         <div className="lg:col-span-6 mb-10">
           <Details doctor={doctor} />
         </div>
         <div className="lg:col-span-3 lg:mx-5 m-auto mt-5 lg:min-w-[370px] w-full">
           <SuggestionList />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
