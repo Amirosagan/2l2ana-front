acba59ffd7b02d51fdb901439fc5afb0872b39cd
Omar Abdelsalam <omar3328101@gmail.com>
2024-09-15 03:51:02 +0200

    user id

--- src/app/[locale]/(users)/booking-Doctor/[id]/page.js
+++ src/app/[locale]/(users)/booking-Doctor/[id]/page.js
@@ -82,15 +82,16 @@ const DoctorDetailsPage = async ({ params }) => {
   return (
     <div dir="rtl" className="p-5 -mt-5 md:mt-0 md:px-32">
       <h2 className="tajawal-bold text-[22px] hidden md:block text-primary">تفاصيل</h2>
-      {/* <div className="grid grid-cols-1 lg:grid-cols-9 gap-5">
+       <div className="grid grid-cols-1 lg:grid-cols-9 gap-5">
         <div className="lg:col-span-6 mb-10">
           <Details doctor={doctor} />
         </div>
-       
-      </div> */}
-       <div className="lg:col-span-3 lg:mx-5 m-auto mt-5 lg:min-w-[370px] w-full">
+        <div className="lg:col-span-3 lg:mx-5 m-auto mt-5 lg:min-w-[370px] w-full">
           <SuggestionList />
         </div>
+       
+      </div> 
+      
     </div>
   );
 };
